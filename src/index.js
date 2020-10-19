cc943d3980461987e47a437fb14c6c9f42a3672d
Aleksey Kulgeyko <kulgeyko93@mail.ru>
2020-10-19 17:53:36 +0300

    docs: complite

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+  let result = (expr.match(/(.{10}|.)/g))
+
+  for (let i = 0; i< result.length; i++) {
+    if( result[i] === '**********') {
+      result[i] = ' ';
+      continue
+    }
+    result[i] = result[i].slice(result[i].indexOf('1'), );
+
+    while (result[i].indexOf('10') !== -1 || result[i].indexOf('11') !== -1) {
+   
+      result[i] = result[i].replace('10', '.');
+      result[i] = result[i].replace('11', '-');
+
+    }
+
+    result[i] = MORSE_TABLE[result[i]];
+  }
+
+  return result.join('')
 }
 
 module.exports = {
